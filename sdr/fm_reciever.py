1f3ab213f2e9d222d055ff85ea1233b2bc065cd9
Jack Quinn <jack.c.quinn@gmail.com>
2019-12-27 08:32:14 -0800

    added hp filter

--- sdr/fm_reciever.py
+++ sdr/fm_reciever.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 ##################################################
 # GNU Radio Python Flow Graph
-# Title: Top Block
-# Generated: Thu Dec 26 21:28:44 2019
+# Title: Fm Reciever
+# Generated: Fri Dec 27 08:28:47 2019
 ##################################################
 
 if __name__ == '__main__':
@@ -34,22 +34,70 @@ import time
 import wx
 
 
-class top_block(grc_wxgui.top_block_gui):
+class fm_reciever(grc_wxgui.top_block_gui):
 
     def __init__(self):
-        grc_wxgui.top_block_gui.__init__(self, title="Top Block")
+        grc_wxgui.top_block_gui.__init__(self, title="Fm Reciever")
         _icon_path = "/usr/share/icons/hicolor/32x32/apps/gnuradio-grc.png"
         self.SetIcon(wx.Icon(_icon_path, wx.BITMAP_TYPE_ANY))
 
         ##################################################
         # Variables
         ##################################################
+        self.variable_slider_2 = variable_slider_2 = 50
+        self.variable_slider_1 = variable_slider_1 = 50
         self.variable_slider_0 = variable_slider_0 = 100.7e6
         self.samp_rate = samp_rate = 1.0e6
 
         ##################################################
         # Blocks
         ##################################################
+        _variable_slider_2_sizer = wx.BoxSizer(wx.VERTICAL)
+        self._variable_slider_2_text_box = forms.text_box(
+        	parent=self.GetWin(),
+        	sizer=_variable_slider_2_sizer,
+        	value=self.variable_slider_2,
+        	callback=self.set_variable_slider_2,
+        	label='hp_width',
+        	converter=forms.float_converter(),
+        	proportion=0,
+        )
+        self._variable_slider_2_slider = forms.slider(
+        	parent=self.GetWin(),
+        	sizer=_variable_slider_2_sizer,
+        	value=self.variable_slider_2,
+        	callback=self.set_variable_slider_2,
+        	minimum=10,
+        	maximum=200,
+        	num_steps=100,
+        	style=wx.SL_HORIZONTAL,
+        	cast=float,
+        	proportion=1,
+        )
+        self.Add(_variable_slider_2_sizer)
+        _variable_slider_1_sizer = wx.BoxSizer(wx.VERTICAL)
+        self._variable_slider_1_text_box = forms.text_box(
+        	parent=self.GetWin(),
+        	sizer=_variable_slider_1_sizer,
+        	value=self.variable_slider_1,
+        	callback=self.set_variable_slider_1,
+        	label='hp_cutoff_freq',
+        	converter=forms.float_converter(),
+        	proportion=0,
+        )
+        self._variable_slider_1_slider = forms.slider(
+        	parent=self.GetWin(),
+        	sizer=_variable_slider_1_sizer,
+        	value=self.variable_slider_1,
+        	callback=self.set_variable_slider_1,
+        	minimum=0,
+        	maximum=200,
+        	num_steps=100,
+        	style=wx.SL_HORIZONTAL,
+        	cast=float,
+        	proportion=1,
+        )
+        self.Add(_variable_slider_1_sizer)
         _variable_slider_0_sizer = wx.BoxSizer(wx.VERTICAL)
         self._variable_slider_0_text_box = forms.text_box(
         	parent=self.GetWin(),
@@ -127,6 +175,8 @@ class top_block(grc_wxgui.top_block_gui):
         )
         self.low_pass_filter_1 = filter.fir_filter_ccf(4, firdes.low_pass(
         	1, samp_rate, 100e3, 10e3, firdes.WIN_KAISER, 6.76))
+        self.high_pass_filter_0 = filter.fir_filter_fff(1, firdes.high_pass(
+        	1, 24e3, variable_slider_1, variable_slider_2, firdes.WIN_HAMMING, 6.76))
         self.audio_sink_0 = audio.sink(24000, '', True)
         self.analog_wfm_rcv_0 = analog.wfm_rcv(
         	quad_rate=250e3,
@@ -137,11 +187,30 @@ class top_block(grc_wxgui.top_block_gui):
         # Connections
         ##################################################
         self.connect((self.analog_wfm_rcv_0, 0), (self.rational_resampler_xxx_0, 0))    
+        self.connect((self.high_pass_filter_0, 0), (self.audio_sink_0, 0))    
+        self.connect((self.high_pass_filter_0, 0), (self.wxgui_fftsink2_0, 0))    
         self.connect((self.low_pass_filter_1, 0), (self.analog_wfm_rcv_0, 0))    
         self.connect((self.low_pass_filter_1, 0), (self.wxgui_fftsink2_1, 0))    
-        self.connect((self.rational_resampler_xxx_0, 0), (self.audio_sink_0, 0))    
-        self.connect((self.rational_resampler_xxx_0, 0), (self.wxgui_fftsink2_0, 0))    
+        self.connect((self.rational_resampler_xxx_0, 0), (self.high_pass_filter_0, 0))    
         self.connect((self.rtlsdr_source_0, 0), (self.low_pass_filter_1, 0))    
+
+    def get_variable_slider_2(self):
+        return self.variable_slider_2
+
+    def set_variable_slider_2(self, variable_slider_2):
+        self.variable_slider_2 = variable_slider_2
+        self._variable_slider_2_slider.set_value(self.variable_slider_2)
+        self._variable_slider_2_text_box.set_value(self.variable_slider_2)
+        self.high_pass_filter_0.set_taps(firdes.high_pass(1, 24e3, self.variable_slider_1, self.variable_slider_2, firdes.WIN_HAMMING, 6.76))
+
+    def get_variable_slider_1(self):
+        return self.variable_slider_1
+
+    def set_variable_slider_1(self, variable_slider_1):
+        self.variable_slider_1 = variable_slider_1
+        self._variable_slider_1_slider.set_value(self.variable_slider_1)
+        self._variable_slider_1_text_box.set_value(self.variable_slider_1)
+        self.high_pass_filter_0.set_taps(firdes.high_pass(1, 24e3, self.variable_slider_1, self.variable_slider_2, firdes.WIN_HAMMING, 6.76))
 
     def get_variable_slider_0(self):
         return self.variable_slider_0
@@ -161,7 +230,7 @@ class top_block(grc_wxgui.top_block_gui):
         self.low_pass_filter_1.set_taps(firdes.low_pass(1, self.samp_rate, 100e3, 10e3, firdes.WIN_KAISER, 6.76))
 
 
-def main(top_block_cls=top_block, options=None):
+def main(top_block_cls=fm_reciever, options=None):
 
     tb = top_block_cls()
     tb.Start(True)
